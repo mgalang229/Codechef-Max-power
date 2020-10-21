1681ed46ed383c512c48eef5e1ac65b42daab6be
Jason Galang <51401355+mgalang229@users.noreply.github.com>
2020-10-21 18:25:30 +0800

    Update sol.cpp

--- sol.cpp
+++ sol.cpp
@@ -1,19 +1,20 @@
 #include <bits/stdc++.h>
+
 using namespace std;
 
-#define ll long long
-
 int main() {
-	ios::sync_with_stdio(0);
+	ios::sync_with_stdio(false);
 	cin.tie(0);
-	
 	int n;
 	cin >> n;
 	string s;
 	cin >> s;
-	int tmp=0;
-	for(int i=0; i<n; ++i)
-		if(s[i]=='1')
-			tmp=i;
-	cout << n-tmp-1 << "\n";
+	int tmp = 0;
+	for (int i = 0; i < n; i++) {
+		if (s[i] == '1') {
+			tmp = i;
+		}
+	}
+	cout << n - tmp - 1 << '\n';
+	return 0;
 }
